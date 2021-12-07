ea17f25aac04674b870e5cee7c684996a8a474b6
Eli Kabasele <eliezerkabas@outlook.com>
2021-12-07 23:17:38 +0100

    create user model

---
+++ src/app/auth/user.models.ts
@@ -0,0 +1,17 @@
+import { tokenize } from '@angular/compiler/src/ml_parser/lexer';
+
+export class User {
+  constructor(
+    public email: string,
+    public id: string,
+    private _token: string,
+    private _tokenExpirationDate: Date
+  ) {}
+
+  get token() {
+    if (!this._tokenExpirationDate || new Date() > this._tokenExpirationDate) {
+      return null;
+    }
+    return this._token;
+  }
+}
